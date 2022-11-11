2efab529a68f6338bb24b879c34679092fdf775c
webee_webs <95363723+webbee-webs@users.noreply.github.com>
2022-11-11 09:56:16 -0300

    Co-authored-by: emiliacasaravilla <emiliacasaravilla@users.noreply.github.com>

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@ const container = document.getElementById('results')
 
 /* --------------------------------- render --------------------------------- */
 
-const render = (arr) => {
+const render = async (arr) => {
     arr = arr.length > 1 ? arr : new Array(arr);
     container.innerHTML = ''
     console.log(arr)
@@ -25,39 +25,60 @@ const render = (arr) => {
 /* -------------------------------- buscador -------------------------------- */
 
 const buscar = async (id = '') => {
-    let result = await fetch(url + id).then(res => res.ok ? res.json() : console.log(res.status)).then(res => res)
+    let result = await fetch(url + id).then(res => res.ok ? res.json() : error()).then(res => res)
     return result
 }
 
 document.getElementById('btnGet1').addEventListener('click', async () => {
     let id = document.getElementById('inputGet1Id')
     let results = await buscar(id.value)
-    console.log('results')
-    console.log(results)
     render(results)
+
 })
+
 
 /* ----------------------------------- put ---------------------------------- */
 
-const putUser = (obj) => {
-    fetch(url, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(obj)
+document.getElementById('btnPut').addEventListener('click', (e) => {
+    putUser(document.getElementById('inputPutId').value)
+})
+
+const putUser = async (id) => {
+    let user = await buscar(id)
+    let inputName = document.getElementById('inputPutNombre')
+    let inputApellido = document.getElementById('inputPutApellido')
+    inputName.value = user.name
+    inputApellido.value = user.lastname
+    document.getElementById('btnSendChanges').addEventListener('click', async () => {
+        if (inputName.value == '' ||
+            inputApellido.value == '') {
+            return
+        }
+        await fetch(url + id, {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'PUT',
+            body: JSON.stringify({
+                name: inputName.value,
+                lastname: inputApellido.value
+            })
+        }).then(res => res.ok ? console.log('todo ok') : error())
+        render(await buscar())
     })
 }
 
 /* ----------------------------- nuevo registro ----------------------------- */
-function registrar(obj) {
-    fetch(url, {
+async function registrar(obj) {
+    await fetch(url, {
         headers: {
             'Content-Type': 'application/json'
         },
         method: 'POST',
         body: JSON.stringify(obj)
     })
+    let data = await buscar()
+    render(data)
 }
 
 document.getElementById('btnPost').addEventListener('click', (e) => {
@@ -66,16 +87,27 @@ document.getElementById('btnPost').addEventListener('click', (e) => {
         name: document.getElementById('inputPostNombre').value,
         lastname: document.getElementById('inputPostApellido').value
     }
-    registrar(nuevo_User);
-    render(traerUsuarios(url));
+    if (nuevo_User.name.trim() == "" || nuevo_User.lastname.trim() == "") {
+        error()
+        return
+    }
+    registrar(nuevo_User)
 })
+
+/* ----------------------------- Disable button ----------------------------- */
 document.getElementById('inputPostNombre').addEventListener('input', (e) => {
     let btnAgregar = document.getElementById('btnPost');
-    e.target.value !== '' ? btnAgregar.removeAttribute('disabled') : btnAgregar.setAttribute('disabled','')
- })
- document.getElementById('inputPostNombre').addEventListener('input', (e) => {
-    
- })
+    e.target.value !== '' ? btnAgregar.removeAttribute('disabled') : btnAgregar.setAttribute('disabled', '')
+})
+document.getElementById('inputPutId').addEventListener('input', (e) => {
+    let btnAgregar = document.getElementById('btnPost');
+    e.target.value !== '' ? btnAgregar.removeAttribute('disabled') : btnAgregar.setAttribute('disabled', '')
+})
+document.getElementById('inputPostApellido').addEventListener('input', (e) => {
+    let btnAgregar = document.getElementById('btnPost');
+    e.target.value !== '' ? btnAgregar.removeAttribute('disabled') : btnAgregar.setAttribute('disabled', '')
+
+})
 
 
 
@@ -83,7 +115,11 @@ document.getElementById('inputPostNombre').addEventListener('input', (e) => {
 
 document.getElementById('inputDelete').addEventListener('input', (e) => {
     let btn = document.getElementById('btnDelete')
-    e.target.value !== '' ? btn.removeAttribute('disabled') : btn.setAttribute('disabled','');
+    e.target.value !== '' ? btn.removeAttribute('disabled') : btn.setAttribute('disabled', '');
+})
+document.getElementById('inputPutId').addEventListener('input', (e) => {
+    let btn = document.getElementById('btnPut')
+    e.target.value !== '' ? btn.removeAttribute('disabled') : btn.setAttribute('disabled', '');
 })
 
 document.getElementById('btnDelete').addEventListener('click', () => {
@@ -92,14 +128,27 @@ document.getElementById('btnDelete').addEventListener('click', () => {
 })
 
 
-async function borrar(id){
+async function borrar(id) {
     await fetch(url + id, {
         method: 'DELETE'
     })
-        .then(response => response.json())
+        .then(res => res.ok ? res.json() : error())
         .then(data => console.log(data));
     let data = await buscar()
     render(data)
 }
+
+
+/* -------------------------------- error -------------------------------- */
+
+function error() {
+
+    document.getElementById('error').classList.add('show');
+    setTimeout(() => {
+        document.getElementById('error').classList.remove('show');
+    }, 3000);
+
+};
+
 
 
